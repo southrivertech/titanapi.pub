0dab64255282d69910c5240af81c3438ecf4b6b3
John Glavin <95938160+srtjohn@users.noreply.github.com>
2023-12-18 19:37:41 -0800

    Add example to list users last login date

---
+++ sdk/powershell/samples/users/user-list-lastlogin.ps1
@@ -0,0 +1,28 @@
+# List all users and their last login date
+$myserver = "myserver"
+
+# get list of users for native authenticator
+$list = Get-UsrList -ServerGuid $myserver -AuthGuid native
+
+$Array = @()
+
+# loop on each user returned
+foreach($user in $list.Response.UserList) {
+
+    # Get user params
+    $upr = Get-UsrParam -ServerGuid $myserver -AuthGuid $user.AuthGuid -UserGuid $user.UserName -byUserName
+    $up = $upr.Response
+
+    # Create row for the output table
+    $Row = "" | Select UserName,Enabled,LastLoginDateUTC,AccountCreated
+    $Row.UserName = $user.UserName
+    $Row.Enabled = $up.Ident.AcctEnabled
+    # convert from Unix EPOCH milliseconds to date time
+    $Row.LastLoginDateUTC = (([System.DateTimeOffset]::FromUnixTimeMilliSeconds($up.Ident.LastLoginUtc)).DateTime)
+    $Row.AccountCreated = (([System.DateTimeOffset]::FromUnixTimeMilliSeconds($up.Ident.AcctCreatedDate)).DateTime)
+    $Array += $Row
+
+}
+
+# Print out nicely formatted table
+$Array
