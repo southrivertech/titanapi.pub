90e484e69afeeed3ac29dc526423e0a5055a7e96
John Glavin <95938160+srtjohn@users.noreply.github.com>
2024-10-08 10:44:52 -0700

    update sample for bulk user homedir to support ldap or adsi authentication

--- sdk/powershell/samples/users/user-bulk-sethomedir.ps1
+++ sdk/powershell/samples/users/user-bulk-sethomedir.ps1
@@ -1,9 +1,13 @@
-$serverName = "leg-titan"    # enter your servername here
-$csvFile = ".\titan-foldermapping.csv"    # this is the csv file which has the syntax [USER:username],homedir path
+$serverName = "dbtest"    # enter your servername here
+$authGuid = "native"
+$authDomainSuffix = "@srtlab.local"    # enter your specific domain suffic if you are not using native authentication
 
 # admin credentials
 $adminUsername = "admin"
 $adminPassword = "test"
+
+# this is the csv file which has the syntax [USER:username],homedir path
+$csvFile = ".\titan-foldermapping.csv"    
 
 # Login to Admin REST API
 $login = Invoke-Login -UserName $adminUsername -PassWord $adminPassword
@@ -13,6 +17,25 @@ if (0 -eq $login.Result.ErrorCode) {
 } else {
     Write-Host "Error: " $login.Result.ErrorCode - $login.Result.ErrorStr
 }
+
+####################################################################################################################################
+# Find the auth guid if needed, for native you can comment out this section
+# If not using native auth then you need to find the auth guid. Finding a specific auth connector by the domain suffix, this is the @domain you used when setting up the auth connector for say NTSAM or LDAP/ADSI etc. It should
+# always be prefixed by an @ sign.  This case below is for an adsi connector defined on the specified server
+$authParams = Get-SvrAuthConnectorParam -AuthGuid $authDomainSuffix -ServerGuid $serverName
+
+Write-Output "Auth params object is as follows:"
+Write-Output ($authParams.Response) | Format-List
+
+# note: for NTSAM you would use the Response.NtsamPoco object instead of AdsiPoco, etc. 
+Write-Host "Auth connector: " $authParams.Response.AdsiPoco.AuthDesc " AuthGUID: "  $authParams.Response.AdsiPoco.AuthGuid " DomainSuffix: " $authParams.Response.AdsiPoco.DomainSuffix
+Write-Host "Auth connector: " $authParams.Response.LdapPoco.AuthDesc " AuthGUID: "  $authParams.Response.LdapPoco.AuthGuid " DomainSuffix: " $authParams.Response.LdapPoco.DomainSuffix
+
+# save the authGuid for future calls to do things with users/groups from this auth connector
+#$authGuid = $authParams.Response.AdsiPoco.AuthGuid
+$authGuid = $authParams.Response.LdapPoco.AuthGuid
+
+##### end of authGUID query, comment out the above section if just using native #####################################################
 
 # Import the CSV file into a PowerShell object
 $data = Import-Csv -Path $csvFile -Header 'User', 'HomeDir'
@@ -36,14 +59,14 @@ foreach ($row in $data) {
         Write-Host "Setting User: " $userName " HomeDir: " $homeDir
    
         # Get the user params for the user
-        $up = Get-UsrParam -ServerGuid $serverName -AuthGuid native -UserGuid $userName -byUserName
+        $up = Get-UsrParam -ServerGuid $serverName -AuthGuid $authGuid -UserGuid $userName -byUserName
         if (0 -eq $up.Result.ErrorCode) {
             # Set users HomeDir to Custom folder
             $up.Response.General.HomeDir = $homeDir
             $up.Response.General.HomeDirInherit = 2    # custom homedir 
 
             # Just update the General Poco
-            $up2 = Set-UsrParam -serverGUID $serverName -authGUID native -userGUID $userName -General $up.Response.General -byUserName
+            $up2 = Set-UsrParam -serverGUID $serverName -authGUID $authGuid -userGUID $userName -General $up.Response.General -byUserName
             Write-Host "Error: " $up2.Result.ErrorCode - $up2.Result.ErrorStr
 
         } else {
